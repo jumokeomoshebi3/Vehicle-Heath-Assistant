74d00e5a80c4fd8f3c1c63ddc5fd3bfaa6136a70
jomoshe1 <jomoshe1@students.towson.edu>
2025-10-21 10:14:35 -0400

    Trained model

--- AI-Health-Assistant/train-model.py
+++ AI-Health-Assistant/train-model.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
+#simulating sensor data for vehile monitoring
 num_samples = 500
 data = {
     "engine_temp": np.random.normal(90, 10, num_samples),
@@ -15,4 +16,37 @@ data = {
 }
 
 df = pd.DataFrame(data)
+
+# Create labels (basic rules for now)
+def label_row(row):
+    if row ["battery voltage"] < 11.5:
+        return "Battery Issue"
+    elif row ["engine_temp"] > 110:
+        return "Overheating"
+    elif row ["tire_pressure"] < 28:
+        return "Low Tire Pressure"
+    elif row ["oil_level"] < 40:
+        return "Low Oil"
+    else:
+        return "Normal :)"
+    
+    df["status"] = df.apply(label_row, axis=1)
+
+# Split data
+X = df.drop("status", axis=1)
+y = df["status"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Trainig the model
+model = DecisionTreeClassifier (max_depth = 4, random_state = 42)
+model.fit(X_train, y_train)
+
+# Evaluating the model
+y_pred = model.predict(X_test)
+print ("Accuracy:", accuracy_score(y_test, y_pred))
+
+#Save the model 
+joblib.dump(model, "vehicle_health_model.pkl")
+print("Model saved as vehicle_health_model.pkl")
+
 
